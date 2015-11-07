8cb66a4ff626c91fce21f20a0dabafe32ac8fbb6
Alik <Ostrovskiyan15@gmail.com>
2015-11-08 00:36:06 +0200

    Fix #57

--- WimK/database/db_wimk.sql
+++ WimK/database/db_wimk.sql
@@ -56,8 +56,10 @@ create table PointType(idType INT, name varchar(10), primary key(idType));
 alter table PointType add unique index (name);
 insert into PointType values(1, 'common');
 insert into PointType values(2, 'sos');
-alter table Point add pointType INT;
-alter table Point add constraint fk_point_type foreign key (pointType) references PointType(idType);
+
+alter table Point drop column pointType;
+alter table Point add idType INT;
+alter table Point add constraint fk_point_type foreign key (idType) references PointType(idType);
+update Point set PointType = 1;
 /*change password type because start use SHA-512*/
 alter table Parent modify password varchar(128);
-update Point set PointType = 1;
