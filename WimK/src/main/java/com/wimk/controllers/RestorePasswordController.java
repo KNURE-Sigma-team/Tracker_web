922c7e0e64f9852f4362630d38d02d5511183e37
Alik <Ostrovskiyan15@gmail.com>
2015-11-07 19:27:39 +0200

    Close #41. Also added new war file for hosting

--- WimK/src/main/java/com/wimk/controllers/RestorePasswordController.java
+++ WimK/src/main/java/com/wimk/controllers/RestorePasswordController.java
@@ -4,12 +4,14 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.wimk.entity.Parent;
+import com.wimk.secure.Sha512Encoder;
 import com.wimk.service.ParentService;
 import com.wimk.utils.EmailSender;
 
@@ -31,7 +33,13 @@ public class RestorePasswordController {
 			model.put("email_not_exist", "No account found with that email address.");
 			return "RestorePassword";
 		}
-		EmailSender.sendRestorePasswordMessage(parent.getLogin(), parent.getPassword());
+		
+		String newPassword = RandomStringUtils.randomAlphanumeric(10);
+		parent.setPassword(new Sha512Encoder().encode(newPassword));
+		parentService.editParent(parent);
+		
+		EmailSender.sendRestorePasswordMessage(parent.getLogin(), newPassword);
+		
 		return "Login";
 	}
 
