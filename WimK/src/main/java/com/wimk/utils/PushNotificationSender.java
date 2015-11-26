4d43bff16407eec2f8a864b4ab8d4ba9b17c2437
Alik <Ostrovskiyan15@gmail.com>
2015-11-26 12:44:14 +0200

    #96. Fixed 'Sending invalid authorizationNumber by mobile authorization'. Fixed 'not sending push notfication to google service'

--- WimK/src/main/java/com/wimk/utils/PushNotificationSender.java
+++ WimK/src/main/java/com/wimk/utils/PushNotificationSender.java
@@ -1,7 +1,9 @@
 package com.wimk.utils;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -12,19 +14,19 @@ public class PushNotificationSender {
 	}
 	
 	private static final String URL = "https://gcm-http.googleapis.com/gcm/send";
-	private static final String AUTHORIZATION_KEY = "https://gcm-http.googleapis.com/gcm/send";
+	private static final String AUTHORIZATION_KEY = "AIzaSyCLk48A05HJnoiQ4OBhTVhVNfsaRPIVv7w";
 	
 	public static void sendPush(String token) throws IOException{
 		URL obj = new URL(URL);
 		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
+		
 		con.setRequestMethod("POST");
 		con.setRequestProperty("Content-Type", "application/json");
 		con.setRequestProperty("Authorization", "key=" + AUTHORIZATION_KEY);
 
 		StringBuilder urlParameters = new StringBuilder();
 		urlParameters.append("{\"data\": {")
-				.append("\"purpose\":\"give point\"")
+				.append("\"purpose\":\"give_point\"")
 				.append("},")
 				.append("\"to\":\"").append(token).append("\"")
 				.append("}");
@@ -35,5 +37,6 @@ public class PushNotificationSender {
 		wr.writeBytes(urlParameters.toString());
 		wr.flush();
 		wr.close();
+		con.getResponseCode();
 	}
 }
